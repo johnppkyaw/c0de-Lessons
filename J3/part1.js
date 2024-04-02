6ebd6b7ee0263f7bd1095b9cd6fb459b14a6c61c
John Kyaw <jjpyae@gmail.com>
2024-04-01 23:10:53 -0700

    complete mergeArrays

---
+++ J3/part1.js
@@ -0,0 +1,8 @@
+function mergeArrays(arr1, arr2) {
+  return arr1.concat(arr2);
+}
+console.log(mergeArrays(['Rattata', 'Raticate'], ['Bulbasaur', 'Ivysaur', 'Venusaur']));
+
+module.exports = {
+  mergeArrays
+}
