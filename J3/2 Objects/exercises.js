d5fe9963f5b4e77f5b4b1d47b16c145466e85a0e
John Kyaw <jjpyae@gmail.com>
2024-04-08 22:46:02 -0700

    complete J3 - 2. Object part 2  exercises

--- J3/2 Objects/exercises.js
+++ J3/2 Objects/exercises.js
@@ -27,10 +27,76 @@ function countOccurrences(arr) {
   return result;
 }
 
+//Write a function called longestString that finds the longest value string in an object.
+function longestString(obj) {
+  result = Object.values(obj).reduce((acc, ele) => {
+    if(ele.length > acc.length) {
+      acc = ele;
+    }
+    return acc;
+  },"");
+  return result;
+}
+
+//Write a function called keyOfLongestString that finds the longest value string but returns its key.
+function keyOfLongestString(obj) {
+  if(Object.keys(obj).length === 0) return undefined;
+  return Object.keys(obj).reduce((acc, ele) => {
+    if(obj[ele].length > obj[acc].length) {
+      acc = ele;
+    }
+    return acc;
+  });
+}
+
+//Use your keyOfLongestString function to write a function called removeLongestString.
+function removeLongestString(obj) {
+  delete obj[keyOfLongestString(obj)];
+}
+
+//Write a function called commas that returns a string of all of an object's values separated by commas.
+function commas(obj) {
+  return Object.values(obj).join(", ");
+}
+
+//Write a function called headers that joins all of an object's keys inside <h1> tags.
+function headers(obj) {
+  return Object.keys(obj).reduce((acc, el) => {
+    acc = acc + `<h1>${el}</h1>`;
+    return acc;
+  }, "");
+}
+
+//Part 2: Modify your function so that it returns both keys and values inside the <h1> tags.
+
+function headers2(obj) {
+  return Object.keys(obj).reduce((acc, el) => {
+    acc = acc + `<h1>${el}: ${obj[el]}</h1>`;
+    return acc;
+  }, "");
+}
+
+//Part 3: Finally, modify your function so that it returns a string of joined keys and values, separated into <div>s and with keys in <h1>s and values in <h2>s.
+function headers3(obj) {
+  return Object.entries(obj).reduce((acc, el) => {
+    acc = acc + `<div><h1>${el[0]}</h1><h2>${el[1]}</h2></div>`;
+    return acc;
+  }, "");
+}
+
+//Write a forEach function for objects.
+Object.prototype.forEach = function(fun, )
 
 module.exports = {
   addKV,
   filterNonKeys,
   addDescriptions,
-  countOccurrences
+  countOccurrences,
+  longestString,
+  keyOfLongestString,
+  removeLongestString,
+  commas,
+  headers,
+  headers2,
+  headers3
 }
--- J3/2 Objects/exercises.test.js
+++ J3/2 Objects/exercises.test.js
@@ -130,3 +130,190 @@ describe('countOccurrences function', () => {
     expect(result).toEqual({})
   })
 })
+
+describe('longestString function', () => {
+  it('should find the longest string from the beginning of an object', () => {
+    const info = {
+      ironman: 'arrogant',
+      spiderman: 'naive',
+      hulk: 'strong'
+    }
+    expect(fn.longestString(info)).toEqual('arrogant')
+  })
+  it('should find the longest string from the end of an object', () => {
+    const leaders = {
+      vermilion: 'Surge',
+      cinnabar: 'Blaine',
+      fuchsia: 'Koga',
+      saffron: 'Sabrina'
+    }
+    expect(fn.longestString(leaders)).toEqual('Sabrina')
+  })
+  it('should return the empty string for an empty object', () => {
+    expect(fn.longestString({})).toEqual('')
+  })
+})
+
+describe('keyOfLongestString function', () => {
+  it('should find key of longest string in the beginning of an object', () => {
+    const info = {
+      ironman: 'arrogant',
+      spiderman: 'naive',
+      hulk: 'strong'
+    }
+    expect(fn.keyOfLongestString(info)).toEqual('ironman')
+  })
+  it('should find key of longest string at the end of an object', () => {
+    const leaders = {
+      vermilion: 'Surge',
+      cinnabar: 'Blaine',
+      fuchsia: 'Koga',
+      saffron: 'Sabrina'
+    }
+    expect(fn.keyOfLongestString(leaders)).toEqual('saffron')
+  })
+  it('should return undefined (no key) for an empty object', () => {
+    expect(fn.keyOfLongestString({})).toEqual(undefined)
+  })
+})
+
+describe('removeLongestString function', () => {
+  it('should remove the longest string in the beginning of an object', () => {
+    const info = {
+      ironman: 'arrogant',
+      spiderman: 'naive',
+      hulk: 'strong'
+    }
+    fn.removeLongestString(info)
+    expect(info).toEqual({ spiderman: 'naive', hulk: 'strong' })
+  })
+  it('should remove the longest string at the end of an object', () => {
+    const leaders = {
+      vermilion: 'Surge',
+      cinnabar: 'Blaine',
+      fuchsia: 'Koga',
+      saffron: 'Sabrina'
+    }
+    fn.removeLongestString(leaders)
+    expect(leaders).toEqual({
+      vermilion: 'Surge',
+      cinnabar: 'Blaine',
+      fuchsia: 'Koga'
+    })
+  })
+  it('should work on an empty object', () => {
+    const imEmpty = {}
+    fn.removeLongestString(imEmpty)
+    expect(imEmpty).toEqual({})
+  })
+})
+
+describe('commas function', () => {
+  it('should separate three items', () => {
+    const info = {
+      ironman: 'arrogant',
+      spiderman: 'naive',
+      hulk: 'strong'
+    }
+    expect(fn.commas(info)).toEqual('arrogant, naive, strong')
+  })
+  it('should put no commas if only one element', () => {
+    expect(fn.commas(['funny'])).toEqual('funny')
+  })
+  it('should return an empty string if no elements', () => {
+    expect(fn.commas([])).toEqual('')
+  })
+})
+
+describe('headers function (part 1)', () => {
+  it('should create h1s for 3 items', () => {
+    const info = {
+      ironman: 'arrogant',
+      spiderman: 'naive',
+      hulk: 'strong'
+    }
+    expect(fn.headers(info)).toEqual(
+      '<h1>ironman</h1><h1>spiderman</h1><h1>hulk</h1>'
+    )
+  })
+  it('should create headers for 4 elements', () => {
+    const leaders = {
+      vermilion: 'Surge',
+      cinnabar: 'Blaine',
+      fuchsia: 'Koga',
+      saffron: 'Sabrina'
+    }
+    expect(fn.headers(leaders)).toEqual(
+      '<h1>vermilion</h1><h1>cinnabar</h1><h1>fuchsia</h1><h1>saffron</h1>'
+    )
+  })
+  it('should return an empty string if no elements', () => {
+    expect(fn.headers([])).toEqual('')
+  })
+})
+
+describe('headers function (part 2)', () => {
+  it('should create h1s for 3 items', () => {
+    const info = {
+      ironman: 'arrogant',
+      spiderman: 'naive',
+      hulk: 'strong'
+    }
+    // The line breaks are just for ease of reading;
+    //   they won't count as part of the expected
+    //   solution since they're escaped with \
+    const exp =
+      '<h1>ironman: arrogant</h1>\
+<h1>spiderman: naive</h1><h1>hulk: strong</h1>'
+    expect(fn.headers2(info)).toEqual(exp)
+  })
+  it('should create headers for 4 elements', () => {
+    const leaders = {
+      vermilion: 'Surge',
+      cinnabar: 'Blaine',
+      fuchsia: 'Koga',
+      saffron: 'Sabrina'
+    }
+    const exp =
+      '<h1>vermilion: Surge</h1>\
+<h1>cinnabar: Blaine</h1><h1>fuchsia: Koga</h1>\
+<h1>saffron: Sabrina</h1>'
+    expect(fn.headers2(leaders)).toEqual(exp)
+  })
+  it('should return an empty string if no elements', () => {
+    expect(fn.headers2([])).toEqual('')
+  })
+})
+
+
+describe('headers function (part 3)', () => {
+  it('should create h1s for 3 items', () => {
+    const info = {
+      ironman: 'arrogant',
+      spiderman: 'naive',
+      hulk: 'strong'
+    }
+    const exp =
+      '<div><h1>ironman</h1><h2>arrogant</h2></div>\
+<div><h1>spiderman</h1><h2>naive</h2></div>\
+<div><h1>hulk</h1><h2>strong</h2></div>'
+    expect(fn.headers3(info)).toEqual(exp)
+  })
+  it('should create headers for 4 elements', () => {
+    const leaders = {
+      vermilion: 'Surge',
+      cinnabar: 'Blaine',
+      fuchsia: 'Koga',
+      saffron: 'Sabrina'
+    }
+    const exp =
+      '<div><h1>vermilion</h1><h2>Surge</h2></div>\
+<div><h1>cinnabar</h1><h2>Blaine</h2></div>\
+<div><h1>fuchsia</h1><h2>Koga</h2></div>\
+<div><h1>saffron</h1><h2>Sabrina</h2></div>'
+    expect(fn.headers3(leaders)).toEqual(exp)
+  })
+  it('should return an empty string if no elements', () => {
+    expect(fn.headers3([])).toEqual('')
+  })
+})
