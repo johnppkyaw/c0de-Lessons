32ed54001c1748aaabcd7b9f1bc52cbcc51329c1
John Kyaw <jjpyae@gmail.com>
2024-04-09 22:27:45 -0700

    complete J3 - 2. Object part 3  exercises

--- J3/2 Objects/exercises.js
+++ J3/2 Objects/exercises.js
@@ -85,7 +85,77 @@ function headers3(obj) {
 }
 
 //Write a forEach function for objects.
-Object.prototype.forEach = function(fun, )
+Object.prototype.forEach = function(fun, i = 0, keys = Object.keys(this)) {
+  if(i === keys.length) return
+  fun(keys[i], this[keys[i]], i, this)
+  return this.forEach(fun, i + 1, keys)
+}
+
+//Write a filter function for objects.
+Object.prototype.filter = function(cb, i = 0, keys = Object.keys(this), resultObj = {}) {
+  if(i === keys.length) return resultObj;
+  if(cb(keys[i], this[keys[i]], i, this)) {
+    resultObj[keys[i]] = this[keys[i]];
+  }
+  return this.filter(cb, i + 1, keys, resultObj);
+}
+
+//Write a reduce function for objects.
+Object.prototype.reduce = function(cb, acc, i = 0, keys = Object.keys(this), ) {
+  if(i === keys.length) return acc;
+  if(acc === undefined) {
+    acc = cb(keys[0], this[keys[0]], i, this);
+    i += 1;
+  } 
+  acc = cb(acc, keys[i], this[keys[i]], i, this);
+  return this.reduce(cb, acc, i + 1, keys);
+}
+
+//Write a getCharCount prototype function for arrays of strings that returns an object of character counts.
+Array.prototype.getCharCount = function() {
+  const eachStringCount = function(map = {}, element, i = 0) {
+    if (i === element.length) return map;
+    map[element[i]] = map[element[i]] || 0;
+    map[element[i]] += 1;
+    return eachStringCount(map, element, i + 1);
+  }
+  return this.reduce((acc, ele) => {
+    return eachStringCount(acc, ele);
+  }, {});
+}
+
+//Write a getMostCommon prototype function for arrays that returns the most common element.
+Array.prototype.getMostCommon = function () {
+  const mapMostFreq = this.reduce((acc, ele) => {
+    const map = acc[0];
+    map[ele] = map[ele] || 0;
+    map[ele] += 1;
+
+    const count = acc[2];
+    if(map[ele] > count) {
+      acc[1] = ele;
+      acc[2] = map[ele];
+    }
+    return acc;
+  }, [{}, null, 0])
+
+  return mapMostFreq[1];
+}
+
+//Write a removeDupes prototype function for arrays that removes duplicate elements from the array.
+Array.prototype.removeDupes = function() {
+  const mappedDupes = this.reduce((acc, ele) => {
+    acc[ele] = acc[ele] || 0;
+    acc[ele]++;
+    return acc;        
+  }, {} );
+
+  const filtered = this.filter(function(ele){
+    return mappedDupes[ele] === 1;
+  });
+  this.length = 0;
+  filtered.forEach(ele => this.push(ele));
+}
 
 module.exports = {
   addKV,
